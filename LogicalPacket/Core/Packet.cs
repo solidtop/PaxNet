c33e807e46c4233112ca93104f3aaa69448c2a7a
solidtop <solidtop.asp@gmail.com>
2025-08-19 13:30:01 +0200

    Add Packet with decoder

---
+++ LogicalPacket/Core/Packet.cs
@@ -0,0 +1,27 @@
+﻿namespace LogicalPacket.Core;
+public readonly struct Packet(PacketHeader header, ReadOnlyMemory<byte> payload)
+{
+    public PacketHeader Header { get; } = header;
+    public ReadOnlyMemory<byte> Payload { get; } = payload;
+}
+
+public readonly struct PacketHeader(PacketType type, PacketFlags flags, uint sequence)
+{
+    public PacketType Type { get; } = type;
+    public PacketFlags Flags { get; } = flags;
+    public uint Sequence { get; } = sequence;
+}
+
+public enum PacketType : byte
+{
+    Connect,
+    Disconnect,
+}
+
+[Flags]
+public enum PacketFlags : byte
+{
+    None,
+    Reliable,
+    Sequenced,
+}
