00c348fe12f62f14b78be59bfa035542b7df3610
solidtop <solidtop.asp@gmail.com>
2025-08-21 19:49:32 +0200

    Rename file & refactor

---
+++ LogicalPacket/Core/PacketCodec.cs
@@ -0,0 +1,33 @@
+﻿using System.Buffers.Binary;
+
+namespace LogicalPacket.Core;
+
+public static class PacketCodec
+{
+    private const int HeaderSize = 6;
+
+    public static bool TryDecode(ReadOnlyMemory<byte> buffer, out Packet packet)
+    {
+        var span = buffer.Span;
+        var type = (PacketType)span[0];
+        var flags = (PacketFlags)span[1];
+        uint seq = BinaryPrimitives.ReadUInt32BigEndian(span.Slice(2, 4));
+
+        var header = new PacketHeader(type, flags, seq);
+        var payload = buffer[HeaderSize..];
+        packet = new Packet(header, payload);
+
+        return true;
+    }
+
+    public static bool TryEncodeHeader(in PacketHeader header, Span<byte> destination, out int bytesWritten)
+    {
+        destination[0] = (byte)header.Type;
+        destination[1] = (byte)header.Flags;
+        BinaryPrimitives.WriteUInt32BigEndian(destination.Slice(2, 4), header.Sequence);
+
+        bytesWritten = HeaderSize;
+
+        return true;
+    }
+}
