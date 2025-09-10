b57e6b6df19b62e1e3203484eaeb510e8b80a7dc
solidtop <solidtop.asp@gmail.com>
2025-09-10 15:21:48 +0200

    Update Demo startup

--- LogicalPacket.Demo/Program.cs
+++ LogicalPacket.Demo/Program.cs
@@ -1,6 +1,22 @@
 ﻿using LogicalPacket;
+using LogicalPacket.Demo;
 
-var server = new Server();
+var netListener = new NetEventListener();
+var server = new Server(netListener);
 server.Start(8000);
-Console.ReadLine();
+
+var cts = new CancellationTokenSource();
+
+_ = Task.Run(() =>
+{
+    Console.ReadLine();
+    cts.Cancel();
+});
+
+while (!cts.Token.IsCancellationRequested)
+{
+    server.PollEvents();
+    await Task.Delay(16);
+}
+
 server.Stop();
