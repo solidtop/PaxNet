cae58d672b28b70bebf43e27c2b085ec1a959b99
solidtop <solidtop.asp@gmail.com>
2025-09-10 15:24:25 +0200

    Add PacketReader with unit tests

---
+++ LogicalPacket/Core/PacketReader.cs
@@ -0,0 +1,101 @@
+using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LogicalPacket.Core;
+
+public ref struct PacketReader(ReadOnlySpan<byte> buffer)
+{
+    private readonly ReadOnlySpan<byte> _buffer = buffer;
+    private int _position;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public byte ReadByte()
+    {
+        var value = _buffer[_position];
+        _position++;
+        return value;
+    }
+
+    public ReadOnlySpan<byte> ReadBytes(int length)
+    {
+        var value = _buffer.Slice(_position, length);
+        _position += length;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public short ReadInt16()
+    {
+        var value = BinaryPrimitives.ReadInt16LittleEndian(_buffer.Slice(_position, 2));
+        _position += 2;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ushort ReadUInt16()
+    {
+        var value = BinaryPrimitives.ReadUInt16LittleEndian(_buffer.Slice(_position, 2));
+        _position += 2;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int ReadInt32()
+    {
+        var value = BinaryPrimitives.ReadInt32LittleEndian(_buffer.Slice(_position, 4));
+        _position += 4;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public uint ReadUInt32()
+    {
+        var value = BinaryPrimitives.ReadUInt32LittleEndian(_buffer.Slice(_position, 4));
+        _position += 4;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public long ReadInt64()
+    {
+        var value = BinaryPrimitives.ReadInt64LittleEndian(_buffer.Slice(_position, 8));
+        _position += 8;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ulong ReadUnt64()
+    {
+        var value = BinaryPrimitives.ReadUInt64LittleEndian(_buffer.Slice(_position, 8));
+        _position += 8;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public float ReadFloat()
+    {
+        var value = BinaryPrimitives.ReadSingleLittleEndian(_buffer.Slice(_position, 4));
+        _position += 4;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public double ReadDouble()
+    {
+        var value = BinaryPrimitives.ReadDoubleLittleEndian(_buffer.Slice(_position, 8));
+        _position += 8;
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public string ReadString()
+    {
+        var length = ReadUInt16();
+        var value = _buffer.Slice(_position, length);
+        _position += length;
+        return Encoding.UTF8.GetString(value);
+    }
+
+    public bool EndOfBuffer => _position == _buffer.Length;
+}
