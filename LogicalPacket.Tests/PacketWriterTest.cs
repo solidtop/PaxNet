1b31367377814d886decf3e6418ef23554f09346
solidtop <solidtop.asp@gmail.com>
2025-09-11 18:10:13 +0200

    Add PacketWriter with unit tests

---
+++ LogicalPacket.Tests/PacketWriterTest.cs
@@ -0,0 +1,114 @@
+using LogicalPacket.Core;
+
+namespace LogicalPacket.Tests;
+
+public class PacketWriterTest
+{
+    [Fact]
+    public void WriteByte_WritesSingleByte()
+    {
+        Span<byte> buffer = stackalloc byte[1];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteByte(0x42);
+
+        var expected = writer.Data;
+        var actual = new byte[] { 0x42 };
+
+        Assert.Equal(actual, expected);
+    }
+
+    [Fact]
+    public void WriteBytes_WritesMultipleBytes()
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        var writer = new PacketWriter(buffer);
+
+        var expected = new byte[] { 1, 2, 3, 4 };
+
+        writer.WriteBytes(expected);
+
+        Assert.Equal(expected, writer.Data);
+    }
+
+    [Fact]
+    public void WriteInt16_WritesLittleEndian()
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteInt16(0x1234);
+
+        Assert.Equal(new byte[] { 0x34, 0x12 }, writer.Data);
+    }
+
+    [Fact]
+    public void WriteUInt16_WritesLittleEndian()
+    {
+        Span<byte> buffer = stackalloc byte[2];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteUInt16(0xABCD);
+
+        Assert.Equal(new byte[] { 0xCD, 0xAB }, writer.Data);
+    }
+
+    [Fact]
+    public void WriteInt32_WritesLittleEndian()
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteInt32(0x12345678);
+
+        Assert.Equal(new byte[] { 0x78, 0x56, 0x34, 0x12 }, writer.Data);
+    }
+
+    [Fact]
+    public void WriteUInt32_WritesLittleEndian()
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteUInt32(0x89ABCDEF);
+
+        Assert.Equal(new byte[] { 0xEF, 0xCD, 0xAB, 0x89 }, writer.Data);
+    }
+
+    [Fact]
+    public void WriteFloat_WritesIEEE754LittleEndian()
+    {
+        Span<byte> buffer = stackalloc byte[4];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteFloat(1.5f);
+
+        // 1.5f in IEEE 754 little-endian: 00 00 C0 3F
+        Assert.Equal(new byte[] { 0x00, 0x00, 0xC0, 0x3F }, writer.Data);
+    }
+
+    [Fact]
+    public void WriteString_WritesLengthAndUtf8Bytes()
+    {
+        Span<byte> buffer = stackalloc byte[10];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteString("Hi");
+
+        // Expected: length (2 bytes LE) + UTF-8 bytes for "Hi"
+        Assert.Equal(new byte[] { 0x02, 0x00, (byte)'H', (byte)'i' }, writer.Data);
+    }
+
+    [Fact]
+    public void MultipleWrites_AppendSequentially()
+    {
+        Span<byte> buffer = stackalloc byte[10];
+        var writer = new PacketWriter(buffer);
+
+        writer.WriteByte(0xAA);
+        writer.WriteInt16(0x1234);
+        writer.WriteByte(0xBB);
+
+        Assert.Equal(new byte[] { 0xAA, 0x34, 0x12, 0xBB }, writer.Data);
+    }
+}
---
+++ LogicalPacket/Core/PacketWriter.cs
@@ -0,0 +1,70 @@
+using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LogicalPacket.Core;
+
+public ref struct PacketWriter(Span<byte> buffer)
+{
+    private readonly Span<byte> _buffer = buffer;
+    private int _position = 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteByte(byte value)
+    {
+        _buffer[_position++] = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteBytes(ReadOnlySpan<byte> values)
+    {
+        values.CopyTo(_buffer.Slice(_position, values.Length));
+        _position += values.Length;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteInt16(short value)
+    {
+        BinaryPrimitives.WriteInt16LittleEndian(_buffer.Slice(_position, 2), value);
+        _position += 2;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteUInt16(ushort value)
+    {
+        BinaryPrimitives.WriteUInt16LittleEndian(_buffer.Slice(_position, 2), value);
+        _position += 2;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteInt32(int value)
+    {
+        BinaryPrimitives.WriteInt32LittleEndian(_buffer.Slice(_position, 4), value);
+        _position += 4;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteUInt32(uint value)
+    {
+        BinaryPrimitives.WriteUInt32LittleEndian(_buffer.Slice(_position, 4), value);
+        _position += 4;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteFloat(float value)
+    {
+        BinaryPrimitives.WriteSingleLittleEndian(_buffer.Slice(_position, 4), value);
+        _position += 4;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void WriteString(string value)
+    {
+        var byteCount = Encoding.UTF8.GetByteCount(value);
+        WriteUInt16((ushort)byteCount);
+        Encoding.UTF8.GetBytes(value, _buffer.Slice(_position, byteCount));
+        _position += byteCount;
+    }
+
+    public Span<byte> Data => _buffer[.._position];
+}
