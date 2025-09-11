ec95449e064cee72d8b0dbd9963dcc04b4c7d78a
solidtop <solidtop.asp@gmail.com>
2025-09-11 13:27:29 +0200

    Simplify server start/stop

--- LogicalPacket.Demo/Program.cs
+++ LogicalPacket.Demo/Program.cs
@@ -5,15 +5,7 @@ var netListener = new NetEventListener();
 var server = new Server(netListener);
 server.Start(8000);
 
-var cts = new CancellationTokenSource();
-
-_ = Task.Run(() =>
-{
-    Console.ReadLine();
-    cts.Cancel();
-});
-
-while (!cts.Token.IsCancellationRequested)
+while (!Console.KeyAvailable)
 {
     server.PollEvents();
     await Task.Delay(16);
