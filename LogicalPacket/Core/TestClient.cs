7b8f006002ce1e5f6be890c964c9db8ff5bc9795
Axel Asp <solidtop.asp@gmail.com>
2025-09-16 08:41:15 +0200

    Minor cleanup to test client

--- LogicalPacket/Core/TestClient.cs
+++ LogicalPacket/Core/TestClient.cs
@@ -5,27 +5,41 @@ using LogicalPacket.Core;
 
 namespace LogicalPacket.Demo;
 
-public class TestClient(string serverAddress, int serverPort)
+public class TestClient(int serverPort)
 {
     private const int MaxPacketSize = 1024;
 
     private readonly Socket _socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-    private readonly IPEndPoint _serverEndPoint = new(IPAddress.Parse(serverAddress), serverPort);
+    private readonly IPEndPoint _serverEndPoint = new(IPAddress.Loopback, serverPort);
     private readonly MemoryPool<byte> _bufferPool = MemoryPool<byte>.Shared;
 
     private CancellationTokenSource? _cts;
     private Task? _receiveTask;
+    private Task? _sendTask;
 
-    public async Task StartAsync()
+    public void Connect(int port)
     {
-        _socket.Bind(new IPEndPoint(IPAddress.Any, 8001));
+        _socket.Bind(new IPEndPoint(IPAddress.Any, port));
         _cts = new CancellationTokenSource();
-
-        await ConnectAsync();
-
+        
         _receiveTask = Task.Run(() => ReceiveLoopAsync(_cts.Token));
-    }
+        
+        // Attempt connection
+        const uint connectionNumber = 1;
+        var connectionTime = DateTime.Now.Ticks;
+        const string key = "MyKey";
 
+        Memory<byte> buffer = new byte[60];
+        var writer = new PacketWriter(buffer.Span);
+
+        writer.WriteByte((byte)PacketType.ConnectRequest);
+        writer.WriteUInt32(connectionNumber);
+        writer.WriteInt64(connectionTime);
+        writer.WriteString(key);
+
+        _socket.SendTo(buffer.Span, SocketFlags.None, _serverEndPoint);
+    }
+    
     internal ValueTask<int> SendAsync(Packet packet, CancellationToken cancellationToken)
     {
         try
@@ -41,22 +55,17 @@ public class TestClient(string serverAddress, int serverPort)
             packet.Dispose();
         }
     }
-
-    private async Task ConnectAsync()
+    
+    private void Shutdown()
     {
-        const uint connectionNumber = 1;
-        var connectionTime = DateTime.Now.Ticks;
-
-        Memory<byte> buffer = new byte[60];
-        var span = buffer.Span;
-        var writer = new PacketWriter(span);
-
-        writer.WriteByte((byte)PacketType.ConnectRequest);
-        writer.WriteUInt32(connectionNumber);
-        writer.WriteInt64(connectionTime);
-        writer.WriteString("MyKey");
-
-        await _socket.SendToAsync(buffer, SocketFlags.None, _serverEndPoint, _cts!.Token);
+        _cts?.Cancel();
+        _receiveTask?.Wait();
+        _sendTask?.Wait();
+        _socket.Close();
+        _cts =  null;
+        _receiveTask = null;
+        
+        Console.WriteLine("Disconnected from server!");
     }
 
     private async Task ReceiveLoopAsync(CancellationToken cancellationToken)
@@ -86,15 +95,31 @@ public class TestClient(string serverAddress, int serverPort)
             }
     }
 
+    private async Task SendLoopAsync(CancellationToken cancellationToken, int delay)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var packet = new Packet(PacketType.Unreliable);
+            packet.Type = PacketType.Unreliable;
+            
+            await SendAsync(packet, cancellationToken);
+            await Task.Delay(delay, cancellationToken);
+        }
+    }
+
     private void HandlePacket(Packet packet)
     {
         switch (packet.Type)
         {
             case PacketType.ConnectAccept:
-                Console.WriteLine("Connected to server!");
+                Console.WriteLine("Connected to server");
+                _sendTask = Task.Run(() => SendLoopAsync(_cts.Token, 100));
+                break;
+            case PacketType.ConnectReject:
+                Shutdown();
                 break;
             case PacketType.Shutdown:
-                Console.WriteLine("Disconnected from server!");
+                Shutdown();
                 break;
             default:
                 Console.WriteLine($"Received packet type: {packet.Type}");
